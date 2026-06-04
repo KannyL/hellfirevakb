f5c00e3049fa99e58de4ed8ccfe33bd27e378625
Athallah-ZET <atalah2009@gmail.com>
2026-06-04 20:56:48 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,33 @@
-document.querySelectorAll('.card-trigger').forEach(item => {
-    item.addEventListener('click', function() {
-        // Cari parent terdekat dari trigger yang diklik
-        const parent = this.closest('.info-card-mini');
-        // Toggle class 'aktif' cuma di parent itu saja
-        parent.classList.toggle('aktif');
+const tombolMenu = document.querySelector('.menu-toggle');
+const kotakMenu = document.querySelector('.dropdown-menu');
+
+if (tombolMenu && kotakMenu) {
+    tombolMenu.addEventListener('click', function(e) {
+        e.stopPropagation(); 
+        if (kotakMenu.style.display === 'block') {
+            kotakMenu.style.display = 'none';
+        } else {
+            kotakMenu.style.display = 'block';
+        }
+    });
+}
+
+window.addEventListener('click', function(e) {
+    if (tombolMenu && kotakMenu) {
+        if (!tombolMenu.contains(e.target) && !kotakMenu.contains(e.target)) {
+            kotakMenu.style.display = 'none';
+        }
+    }
+});
+
+
+const semuaTrigger = document.querySelectorAll('.card-trigger');
+
+semuaTrigger.forEach(trigger => {
+    trigger.addEventListener('click', function() {
+        
+        const cardInduk = this.parentElement;
+        
+        cardInduk.classList.toggle('aktif');
     });
 });
