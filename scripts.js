4b4ce836cc0e8e367dae237b936b4fbb91713483
Athallah-ZET <atalah2009@gmail.com>
2026-06-04 18:05:16 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
+const tombolMenu = document.querySelector('.menu-toggle');
+const kotakMenu = document.querySelector('.dropdown-menu');
 
+tombolMenu.addEventListener('click', function(e) {
+    e.stopPropagation(); 
+    
+    if (kotakMenu.style.display === 'block') {
+        kotakMenu.style.display = 'none';
+    } else {
+        kotakMenu.style.display = 'block';
+    }
+});
+
+// 2. Klik di area luar mana saja -> Otomatis Tutup
+window.addEventListener('click', function(e) {
+    if (!tombolMenu.contains(e.target) && !kotakMenu.contains(e.target)) {
+        kotakMenu.style.display = 'none';
+    }
+});
