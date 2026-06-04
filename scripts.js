35e4bdff1a1ba59f40157daf782220ea3746fbc9
Athallah-ZET <atalah2009@gmail.com>
2026-06-04 19:20:19 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,33 @@
 const tombolMenu = document.querySelector('.menu-toggle');
 const kotakMenu = document.querySelector('.dropdown-menu');
 
-tombolMenu.addEventListener('click', function(e) {
-    e.stopPropagation(); 
-    
-    if (kotakMenu.style.display === 'block') {
-        kotakMenu.style.display = 'none';
-    } else {
-        kotakMenu.style.display = 'block';
+if (tombolMenu && kotakMenu) {
+    tombolMenu.addEventListener('click', function(e) {
+        e.stopPropagation(); 
+        if (kotakMenu.style.display === 'block') {
+            kotakMenu.style.display = 'none';
+        } else {
+            kotakMenu.style.display = 'block';
+        }
+    });
+}
+
+window.addEventListener('click', function(e) {
+    if (tombolMenu && kotakMenu) {
+        if (!tombolMenu.contains(e.target) && !kotakMenu.contains(e.target)) {
+            kotakMenu.style.display = 'none';
+        }
     }
 });
 
-// 2. Klik di area luar mana saja -> Otomatis Tutup
-window.addEventListener('click', function(e) {
-    if (!tombolMenu.contains(e.target) && !kotakMenu.contains(e.target)) {
-        kotakMenu.style.display = 'none';
-    }
+
+const semuaTrigger = document.querySelectorAll('.card-trigger');
+
+semuaTrigger.forEach(trigger => {
+    trigger.addEventListener('click', function() {
+        
+        const cardInduk = this.parentElement;
+        
+        cardInduk.classList.toggle('aktif');
+    });
 });
